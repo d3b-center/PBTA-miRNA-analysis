97d1b208224b8e2a73d4a0920c05962b9d6d9aa9
shehbeel <40303039+shehbeel@users.noreply.github.com>
2023-02-15 22:29:54 -0500

    Added comments

--- utils/sankey-plot.R
+++ utils/sankey-plot.R
@@ -1,9 +1,18 @@
+# Sankey Plot
+# Author: Shehbeel Arif
+# Children's Hospital of Philadelphia
+
+# Load library
 library(plotly)
 
+# Build Sankey plot
 fig <- plot_ly(
+  # Specify Sankey plot
   type = "sankey",
+  # Display plot horizontally
   orientation = "h",
   
+  # Create nodes
   node = list(
     label = c("PBTA", "ATRT", "Cranio", "Ependy", "Gangli", "GNT", "HGAT", "LGAT", "Medullo", "Schwan", "Tert",
               "G1", "G2"),
@@ -15,18 +24,26 @@ fig <- plot_ly(
       width = 0.5
     )
   ),
-  
+  # Specify links between nodes
   link = list(
+    # source nodes
     source = c(0,0,0,0,0,0,0,0,0,0,4,4),
-    target = c(1,2,3,4,5,6,7,8,9,10,11,12,13),
-    value =  c(17,21,36,19,3,34,102,35,2,1,10,9)
+    # target nodes
+    target = c(1,2,3,4,5,6,7,8,9,10,11,12,13), 
+    # size of links
+    value =  c(17,21,36,19,3,34,102,35,2,1,10,9) # size of link
   )
 )
+
+# Specify plot layout
 fig <- fig %>% layout(
+  # Plot title
   title = "Pediatric Brain Tumor Subtyping by miRNA",
+  # Font size
   font = list(
     size = 10
   )
 )
 
+# Display figure
 fig
