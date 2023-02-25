f7db4701a0c75442da3dd40c024cd7de143b3485
shehbeel <40303039+shehbeel@users.noreply.github.com>
2023-02-24 21:45:48 -0500

    Updated survival analysis and added forest plot

--- analyses/03-survival-analysis/code/01-survival-analysis_clusters.R
+++ analyses/03-survival-analysis/code/01-survival-analysis_clusters.R
@@ -49,10 +49,12 @@ metadata_updated <- readr::read_csv(metadata_file) %>%
   distinct(Sample_ID, .keep_all = TRUE) %>%
   # Add Clusters
   inner_join(clusterdata, by="Sample_ID") %>%
+  # Change OS_days to OS_years
+  mutate(OS_years = OS_days / 365.25) %>%
   # Convert clusters to factors
   mutate_at(c("Cluster"), as.factor) %>%
   # Keep only necessary columns
-  select("Sample_ID", "short_histology", "OS_status", "OS_days", "PFS_days", "Cluster")
+  select("Sample_ID", "short_histology", "OS_status", "OS_days", "OS_years", "PFS_days", "Cluster")
 
 ## KAPLAN-MEIER FOR CLUSTERS
 kap_fit <- survival_analysis(
@@ -102,17 +104,48 @@ cowplot::save_plot(filename = kap_meier_plot_file, plot = surv_plot_all_subtype,
 # Save the model itself as well as all other output from `survival_analysis()`
 readr::write_rds(kap_fit, kap_meier_model_file)
 
-
+################################################################################
 ## Analysis: cox regression based on cluster membership
-# Run model
-fit_save_model(metadata_updated, 
-               "Cluster",
-               file.path(results_dir, "cox_cluster_membership.RDS"))
+# Cox model based on Cluster membership
+fit_save_model(df=metadata_updated, 
+               terms="Cluster",
+               output_file=file.path(results_dir, "cox_cluster.RDS"),
+               model_type="multivariate",
+               test="cox")
 
-# ERRORRRRR
-# The fit_save_model() function in survival_models.R is not universal
+# Intersection model: "Cluster*short_histology"
+int_model <- fit_save_model(df=metadata_updated, 
+               terms="Cluster*short_histology",
+               output_file=file.path(results_dir, "cox_intersection_terms_cluster_short_histology.RDS"),
+               model_type="multivariate",
+               test="cox")
 
 
+# Additive model: "Cluster+short_histology"
+add_model <- fit_save_model(df=metadata_updated, 
+               terms="Cluster+short_histology",
+               output_file=file.path(results_dir, "cox_additive_terms_cluster_short_histology.RDS"),
+               model_type="multivariate",
+               test="cox")
 
+################################################################################
+## Pairwise comparison
+# Double check the function isn't missing them - get the same result below
+metadata_recode <- metadata_updated %>%
+  mutate(OS_status = ifelse(OS_status == "LIVING", 0, 1))
+pairwise_survdiff(formula = Surv(OS_years, OS_status) ~ Cluster, 
+                  data = metadata_recode)
+
+################################################################################
+## Export table for manuscript
+# Combine and output
+list_s2_table <- list(kaplan_meier_fit = as.data.frame(kap_fit_data),
+                      kaplan_meier_summary = as.data.frame(kap_fit_data_summary),
+                      cox_additive_model = as.data.frame(add_model)
+)
+openxlsx::write.xlsx(list_s2_table,
+                     table_s2_file, 
+                     overwrite = T, 
+                     keepNA=TRUE)
 
 
